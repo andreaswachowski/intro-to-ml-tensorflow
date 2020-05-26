3eaeae989ab0733177d6f2bbbb8fb7b544e35a2d
Andreas Wachowski <andreas.wachowski@gmail.com>
2020-05-26 13:09:41 +0200

    feat: Add command line tool predict.py

---
+++ projects/p2_image_classifier/predict.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import os
+import sys
+
+from predict_utils import predict, load_model
+
+
+def exit_if_not_file(filename):
+    if not os.path.isfile(filename):
+        print(f"File {filename} not found, aborting.")
+        sys.exit(1)
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser(
+        description="Predict the top flower names from an image along with their corresponding probabilities."
+    )
+    parser.add_argument(
+        "--top_k",
+        metavar="K",
+        type=int,
+        default=5,
+        help="Return the top K most likely classes. Defaults to 5.",
+    )
+    parser.add_argument(
+        "--category_names",
+        metavar="JSON_FILE",
+        type=str,
+        help="Path to a JSON file mapping labels to flower names",
+    )
+
+    parser.add_argument(
+        "image", help="Path to the image of the flower that shall be predicted.",
+    )
+
+    parser.add_argument(
+        "saved_model", help="Path to the Keras model used for prediction.",
+    )
+
+    return parser
+
+
+def main():
+    args = create_argument_parser().parse_args()
+
+    image_path = args.image
+    model_path = args.saved_model
+    category_names_path = args.category_names
+    top_k = args.top_k
+
+    exit_if_not_file(image_path)
+    exit_if_not_file(model_path)
+
+    class_names = None
+    if category_names_path is not None:
+        exit_if_not_file(category_names_path)
+        with open(category_names_path, "r") as f:
+            class_names = json.load(f)
+
+    model = load_model(model_path)
+
+    probs, classes = predict(image_path, model, top_k)
+
+    print(f"\nThe top {top_k} predictions for {image_path} are:\n")
+    for prob, label_index in zip(probs, classes):
+        if class_names is not None:
+            class_name = class_names[label_index]
+        else:
+            class_name = label_index
+        print(f"{prob:>7.3%} {class_name}")
+
+
+main()
---
+++ projects/p2_image_classifier/predict_utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+from PIL import Image
+
+
+def process_image(image):
+    image_size = 224
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, (image_size, image_size))
+    image /= 255
+    return image.numpy()
+
+
+def predict_single_image(image_path, model):
+    image = Image.open(image_path)
+    image = np.asarray(image)
+    processed_image = np.expand_dims(process_image(image), axis=0)
+    predictions = model.predict(processed_image)
+    return predictions[0]
+
+
+def predict(image_path, model, top_k):
+    predictions = predict_single_image(image_path, model)
+    top_k_indices = np.argsort(-predictions)[:top_k]
+    classes = list(
+        map(str, top_k_indices + 1)
+    )  # +1 because labels are 1-based, arrays are 0-based
+    probs = predictions[top_k_indices]
+    return probs, classes
+
+
+def load_model(model_path):
+    return tf.keras.models.load_model(
+        model_path, custom_objects={"KerasLayer": hub.KerasLayer}
+    )
